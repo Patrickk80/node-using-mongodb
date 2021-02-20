1459ab709906e55afae6d659b49f543906fcd14f
Patrick <pwakooijman@gmail.com>
2021-02-20 22:40:07 +0100

    complete module

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 4000;
 
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
@@ -15,3 +17,14 @@ app.get('/', (req, res) =>
 app.listen(PORT, () => 
     console.log(`Your server is running on port ${PORT}`)
 );
+
+mongoose.connect('mongodb://localhost/productsdb', {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connection started')
+});
